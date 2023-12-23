680352b10e6bfff86972b7d67d10664cfc8befb7
showeech21 <91574414+showeech21@users.noreply.github.com>
2023-12-23 15:21:28 +0300

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *systemWork(void *arg) {
+    // Непосредственная работа системы
+    while (1) {
+        // Работа системы
+        printf("Выполняется непосредственная работа системы...\n");
+        // Можно также записывать результаты в файл
+        FILE *fp;
+        fp = fopen("system_log.txt", "a");
+        if (fp != NULL) {
+            fprintf(fp, "Выполняется непосредственная работа системы...\n");
+            fclose(fp);
+        }
+        sleep(5);  // Подождать 5 секунд
+    }
+    pthread_exit(NULL);
+}
+
 
